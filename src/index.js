f9dfe55fa1411c02b3f78ce8ae2ba8860e655218
Sergey978 <vasilevsky.s@gmail.com>
2019-02-06 18:29:08 +0200

    send data to app experiment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndexpApp from './components/index-app';
 
+const testData = {idTest: 'abracadabra'};
 
-ReactDOM.render(<IndexpApp />,
+
+ReactDOM.render(<IndexpApp data = {testData} />,
   document.getElementById('root'));
