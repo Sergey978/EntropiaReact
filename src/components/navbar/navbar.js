a2033ec7db20b9628efbb6ef37403aede6a71457
Sergey978 <vasilevsky.s@gmail.com>
2019-04-23 17:48:40 +0300

    Navbar as react component work now

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Navbar = (data) => {
-   const pageData = data.servData.page;
-   const userData = data.servData.user; 
+const Navbar = () => {   
+   const pageData = viewBag.page;
+   const userData = viewBag.user; 
     return (
-        <div className="navbar navbar-inverse navbar-fixed-top headroom" id="navbar">
+       
             <div className="container">
                 <div className="navbar-header">
 
@@ -13,28 +13,27 @@ const Navbar = (data) => {
                         <span className="icon-bar"></span>
                         <span className="icon-bar"></span>
                     </button>
-                    <a className="navbar-brand" href="#"><img src="../assets/images/logo.png" alt="Progressus HTML5 template" /></a>
+                    <a className="navbar-brand" href="/"><img src="../assets/images/logo.png" alt="Progressus HTML5 template" /></a>
                 </div>
                 <div className="navbar-collapse collapse">
-                    <ul className="nav navbar-nav pull-right">                         
-                        <li className={( pageData.pageName =="Home")?("active"):("")}><a href="#">Home</a></li>
-                        <li className={(pageData.pageName =="About")?("active"):("")}><a href="about.html">About</a></li>
+                    <ul className="nav navbar-nav pull-right">                                           
+                        <li className={( pageData.pageName =="Home")?("active"):("")}><a href="/">Home</a></li>
+                        <li className={(pageData.pageName =="About")?("active"):("")}><a href="/home/about">About</a></li>
                         <li className="dropdown">
                             <a href="#" className="dropdown-toggle" data-toggle="dropdown">More Pages <b className="caret"></b></a>
                             <ul className="dropdown-menu">
-                                <li><a href="#">Left Sidebar</a></li>
-                                <li><a href="#">Right Sidebar</a></li>
+                                <li><a href="/left-sidebar">Left Sidebar</a></li>
+                                <li><a href="/right-sidebar">Right Sidebar</a></li>
                             </ul>
                         </li>
-                        <li className={(pageData.pageName =="Contact")?("active"):("")}><a href="contact.html">Contact</a></li>
-                      
-                      <li><a className="btn" href="#">{userData.userName.trim() === "" ? 'SIGN IN / SIGN UP':userData.userName} </a></li>
+                        <li className={(pageData.pageName =="Contact")?("active"):("")}><a href="/home/contact">Contact</a></li>                      
+                      <li><a className="btn" href="/manage">{userData.userName.trim() === "" ? 'SIGN IN / SIGN UP':userData.userName} </a></li>
                        
                     </ul>
                 </div>
 
             </div>
-        </div>
+       
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndexpApp from './components/index-app';
 
-ReactDOM.render(<IndexpApp data = {jsData} />,
+ReactDOM.render(<IndexpApp data = {viewBag} />,
   document.getElementById('root'));
