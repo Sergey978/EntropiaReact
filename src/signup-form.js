17d4d4ff36098a2a48b50d51ba6f1b0f65d745ed
Sergey978 <vasilevsky.s@gmail.com>
2019-05-16 17:56:44 +0300

    separate signup form from

---
+++ src/signup-form.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SignupForm from './components/forms/signup-form.jsx';
+
+
+ReactDOM.render(<SignupForm />,
+  document.getElementById('signUpForm'));
